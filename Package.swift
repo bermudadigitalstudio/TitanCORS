5d0daeed0effd040f89cfd2b09c7b5a1cbc62bed
Swizzlr <git@swizzlr.co>
2017-01-25 18:00:35 -0500

    Add TitanCore dependency.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "TitanCORS"
+  name: "TitanCORS",
+  dependencies: [
+   .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 2)
+  ]
 )
